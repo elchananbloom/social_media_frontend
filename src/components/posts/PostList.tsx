4067fa0cb914a73a387ed47c94a6b137833494b3
thomasliu2019 <thomasyundaliu@gmail.com>
2026-02-05 13:52:39 -0500

    Update PostList.tsx

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -8,6 +8,7 @@ type PostListProps = {
   onDelete: (id: number) => Promise<void> | void;
   currentUsername: string | null;
   onComment: (id: number) => void;
+  onToggleLike: (id: number) => void;
 };
 
 export default function PostList({
@@ -17,6 +18,7 @@ export default function PostList({
   onDelete,
   currentUsername,
   onComment,
+  onToggleLike,
 }: Readonly<PostListProps>) {
   return (
     <div>
@@ -33,6 +35,9 @@ export default function PostList({
             onSelect={() => onSelect(p.id)}
             onDelete={canDelete ? () => onDelete(p.id) : undefined}
             onComment={() => onComment(p.id)}
+            likes={p.likesCount}
+            likedByCurrentUser={p.isLikedByCurrentUser}
+            onToggleLike={() => onToggleLike(p.id)} 
           />
         );
       })}
